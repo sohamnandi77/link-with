34fdcff545ed3da56e133accc0dae1c0e92944e0
Soham Nandi <sohamnandi77@gmail.com>
2024-09-15 19:14:27 +0530

    add logging to middleware and parse utility for better debugging

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,11 +42,13 @@ export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   // for App
   if (APP_HOSTNAMES.has(domain)) {
+    console.log("app middleware");
     return AppMiddleware(req);
   }
 
   // for API
   if (API_HOSTNAMES.has(domain)) {
+    console.log("api middleware");
     return ApiMiddleware(req);
   }
 
@@ -55,14 +57,17 @@ export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
     domain === env.NEXT_PUBLIC_APP_DOMAIN &&
     DEFAULT_REDIRECTS[key as keyof typeof DEFAULT_REDIRECTS]
   ) {
+    console.log("default redirect");
     return NextResponse.redirect(
       DEFAULT_REDIRECTS[key as keyof typeof DEFAULT_REDIRECTS],
     );
   }
 
   if (isValidUrl(fullKey)) {
+    console.log("link middleware");
     return CreateLinkMiddleware(req);
   }
 
+  console.log("link middleware");
   return LinkMiddleware(req);
 }
