e3f84dc990e7a51242e54bf85d6aca7bb53fed95
Soham Nandi <sohamnandi77@gmail.com>
2024-09-20 02:04:34 +0530

    refactor UrlRedirectPage: uncomment and restore URL handling functions for improved functionality

--- src/app/(links)/default/page.tsx
+++ src/app/(links)/default/page.tsx
@@ -1,42 +1,44 @@
 "use client";
 
-// import { DeepLinker } from "@/lib/deeplinker";
-// import { constructStoreUrl } from "@/lib/functions/construct-store-url";
+import { useLayoutEffect } from "react";
 
-// const handleStoreRedirectionFallback = (url: string, storeUrl: string) => {
-//   try {
-//     const storeDeepLink = constructStoreUrl(storeUrl);
-//     const storeLinker = new DeepLinker({
-//       onIgnored: () => {
-//         window.location.href = url;
-//       },
-//       onFallback: () => {
-//         window.location.href = url;
-//       },
-//     });
-//     storeLinker.openURL(storeDeepLink);
-//   } catch {
-//     window.location.href = url;
-//   }
-// };
+import { DeepLinker } from "@/lib/deeplinker";
+import { constructStoreUrl } from "@/lib/functions/construct-store-url";
 
-// const handleDeeplinkFallback = (
-//   url: string,
-//   deeplink: string,
-//   storeUrl: string,
-// ) => {
-//   const linker = new DeepLinker({
-//     onIgnored: () => {
-//       if (storeUrl) handleStoreRedirectionFallback(url, storeUrl);
-//       else window.location.href = url;
-//     },
-//     onFallback: () => {
-//       if (storeUrl) handleStoreRedirectionFallback(url, storeUrl);
-//       else window.location.href = url;
-//     },
-//   });
-//   linker.openURL(deeplink);
-// };
+const handleStoreRedirectionFallback = (url: string, storeUrl: string) => {
+  try {
+    const storeDeepLink = constructStoreUrl(storeUrl);
+    const storeLinker = new DeepLinker({
+      onIgnored: () => {
+        window.location.href = url;
+      },
+      onFallback: () => {
+        window.location.href = url;
+      },
+    });
+    storeLinker.openURL(storeDeepLink);
+  } catch {
+    window.location.href = url;
+  }
+};
+
+const handleDeeplinkFallback = (
+  url: string,
+  deeplink: string,
+  storeUrl: string,
+) => {
+  const linker = new DeepLinker({
+    onIgnored: () => {
+      if (storeUrl) handleStoreRedirectionFallback(url, storeUrl);
+      else window.location.href = url;
+    },
+    onFallback: () => {
+      if (storeUrl) handleStoreRedirectionFallback(url, storeUrl);
+      else window.location.href = url;
+    },
+  });
+  linker.openURL(deeplink);
+};
 
 const getCookie = (name: string): string | undefined => {
   if (typeof document === "undefined") return undefined;
@@ -50,20 +52,15 @@ export default function UrlRedirectPage() {
   const deeplink = decodeURIComponent(getCookie("deeplink") ?? "");
   const storeUrl = decodeURIComponent(getCookie("store") ?? "");
 
-  // const searchParams = useSearchParams();
-  // const url = searchParams?.get("url") ?? "";
-  // const deeplink = searchParams?.get("deeplink") ?? "";
-  // const storeUrl = searchParams?.get("store") ?? "";
-
-  // useLayoutEffect(() => {
-  //   if (deeplink) {
-  //     handleDeeplinkFallback(url, deeplink, storeUrl);
-  //   } else if (storeUrl) {
-  //     handleStoreRedirectionFallback(url, storeUrl);
-  //   } else {
-  //     window.location.href = url;
-  //   }
-  // }, [deeplink, storeUrl, url]);
+  useLayoutEffect(() => {
+    if (deeplink) {
+      handleDeeplinkFallback(url, deeplink, storeUrl);
+    } else if (storeUrl) {
+      handleStoreRedirectionFallback(url, storeUrl);
+    } else {
+      window.location.href = url;
+    }
+  }, [deeplink, storeUrl, url]);
 
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center">
